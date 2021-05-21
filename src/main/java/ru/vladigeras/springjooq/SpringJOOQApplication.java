50edfecca3d872c4b907624e2e2aea1f5e9a5a98
Vladislav Gerasimov <gerasimov.v88@gmail.com>
2021-05-21 22:16:51 +0300

    Initial commit

---
+++ src/main/java/ru/vladigeras/springjooq/SpringJOOQApplication.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springjooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJOOQApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJOOQApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vladigeras/springjooq/SpringJOOQApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vladigeras.springjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJOOQApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
